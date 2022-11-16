5773e36e73cbe93e7b8aaa58bffa42a9ccfa28ad
prashantdon <114726053+prashantdon@users.noreply.github.com>
2022-11-16 23:35:36 +0530

    class assignment increasing the size of an element

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-const theNumber = document.getElementById('the-number');
-const increase = document.getElementById('increment');
-const decrease = document.getElementById('decrement');
-const reset = document.getElementById('reset-to-zero');
+const black = document.getElementById('div1');
+const green = document.getElementById('div2');
 
-let counter = 0;
-theNumber.innerText = counter;
 
-function incrementBy1() {
-    counter++
-    theNumber.innerText = counter;
-}
-function decrementBy1() {
-    counter--
-    theNumber.innerText = counter;
-}
-function resetToZero(){
-    counter = 0;
-    theNumber.innerText = counter;
-}
-
-increase.addEventListener('click', incrementBy1);
-decrease.addEventListener('click', decrementBy1);
-reset.addEventListener('click', resetToZero);
+green.addEventListener('mousemove',(event)=>{
+    const {x} = event;
+    green.style.width = `${x/4}1px `
+    console.log(event)
+})
